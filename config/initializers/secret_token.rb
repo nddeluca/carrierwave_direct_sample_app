a6b881edc3361192dffec6072dca40959a67646a
Nick DeLuca <nick@wescrimmage.com>
2013-10-08 22:08:33 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarrierwaveDirectSampleApp::Application.config.secret_key_base = '8b10137945683ab7ad339ad4d00ddeacce8ddea6ec39827c663d478bc5ebcd4cac8040a4f98aa11b48e35bebc62567fc9cfa339cbaf0e006131de031b83ee878'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CarrierwaveDirectSampleApp::Application.config.session_store :cookie_store, key: '_carrierwave_direct_sample_app_session'
